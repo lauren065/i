a0dbebe37e67cefa68b4404e533ca69e0fb69e39
lauren065 <lauren065@naver.com>
2026-04-18 19:11:24 +0900

    revert: keep original favicon.ico (unwanted change)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="alternate icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#0a0a0a" />
       </Head>
       <body style={{ backgroundColor: "#0a0a0a" }}>
